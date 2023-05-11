f9e322d1e7a4ec6b68b599f5191672aca8038ea0
asmraihan <asmraihanctg14@gmail.com>
2023-05-11 18:20:38 +0600

    useLayoutEffect

--- src/components/TextSphere.jsx
+++ src/components/TextSphere.jsx
@@ -1,20 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useLayoutEffect } from 'react';
 import TagCloud from "TagCloud";
 const TextSphere = () => {
 
-     // Animation settings for Text Cloud
-  useEffect(() => {
+
+  useLayoutEffect(() => {
     return () => {
       const container = ".tagcloud";
       const texts = [
         "HTML",
         "CSS",
-        "SASS",
-        "JavaScript",
+        "SCSS",
         "Redux",
         "Bootstrap",
         "Next",
-        "NodeJS",
+        "MongoDB",
+        "Sanity",
+        "React Native",
+        "Firebase",
+        "Git",
+        "GitHub",
+        "Heroku",
         "JSON",
         "ES6",
         "VSCode",
@@ -31,9 +36,9 @@ const TextSphere = () => {
       TagCloud(container, texts, options);
     };
   }, []);
-    return (
-        <div className="tagcloud w-[100px] lg:w-full h-[100px] lg:h-full absolute -left-36 lg:left-0 lg:-top-36"></div>    
-    );
+  return (
+    <div className="tagcloud w-[100px] lg:w-full h-[100px] lg:h-full absolute -left-36 lg:left-0 lg:-top-36"></div>
+  );
 };
 
 export default TextSphere;
